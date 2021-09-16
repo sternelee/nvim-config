ea99a5840667705ca967b6ec8b1c1632811967c8
李兴广 <lixingguang@xunlei.com>
2021-09-16 14:34:07 +0800

    update

--- init.lua
+++ init.lua
@@ -7,7 +7,7 @@ local nvim_exec = vim.api.nvim_exec
 local remap = vim.api.nvim_set_keymap
 --gui
 -- g.neovide_fullscreen = true
-g.neovide_cursor_vfx_mode = "pixiedust"
+-- g.neovide_cursor_vfx_mode = "pixiedust"
 nvim_exec([[set guifont=VictorMono\ NF:h16]], false)
 --Install packer
 local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
@@ -21,11 +21,11 @@ require('packer').startup(function()
   use 'wbthomason/packer.nvim'
   use 'nvim-lua/plenary.nvim'
   -- 状态栏
-  use {'windwp/windline.nvim', requires = {'kyazdani42/nvim-web-devicons', opt = true}}
+  use {'famiu/feline.nvim', requires = {'kyazdani42/nvim-web-devicons', opt = true}}
   use 'romgrk/barbar.nvim'
   use 'kyazdani42/nvim-tree.lua'
   use 'glepnir/dashboard-nvim'
-  use 'SmiteshP/nvim-gps'
+  -- use 'SmiteshP/nvim-gps'
   -- git相关
   use 'lewis6991/gitsigns.nvim'
   use 'tpope/vim-fugitive'
@@ -850,206 +850,136 @@ g.dashboard_custom_section = {
     g = {description = {'  Open Neovim Configuration      SPC f e'}, command = ':e ~/AppData/Local/nvim/init.lua'},
 }
 
-local windline = require('windline')
-local helper = require('windline.helpers')
-local b_components = require('windline.components.basic')
-local state = _G.WindLine.state
-local gps = require('nvim-gps')
-local lsp_comps = require('windline.components.lsp')
-local git_comps = require('windline.components.git')
 
-local hl_list = {
-    Black = { 'white', 'black' },
-    White = { 'black', 'white' },
-    Inactive = { 'InactiveFg', 'InactiveBg' },
-    Active = { 'ActiveFg', 'ActiveBg' },
-}
-local basic = {}
+-- feline config
+local get_diag = function(str)
+  local count = vim.lsp.diagnostic.get_count(0, str)
+  return (count > 0) and ' '..count..' ' or ''
+end
 
-local breakpoint_width = 90
-basic.divider = { b_components.divider, '' }
-basic.bg = { ' ', 'StatusLine' }
+local vi_mode_provider = function()
+    local mode_alias = {
+      n = 'NORMAL',
+      no = 'NORMAL',
+      i = 'INSERT',
+      v = 'VISUAL',
+      V = 'V-LINE',
+      [''] = 'V-BLOCK',
+      c = 'COMMAND',
+      cv = 'COMMAND',
+      ce = 'COMMAND',
+      R = 'REPLACE',
+      Rv = 'REPLACE',
+      s = 'SELECT',
+      S = 'SELECT',
+      [''] = 'SELECT',
+      t = 'TERMINAL',
+    }
+    return ' ' .. mode_alias[vim.fn.mode()] .. ' '
+end
 
-local colors_mode = {
-    Normal = { 'red', 'black' },
-    Insert = { 'green', 'black' },
-    Visual = { 'yellow', 'black' },
-    Replace = { 'blue_light', 'black' },
-    Command = { 'magenta', 'black' },
-}
+local percentage_provider = function()
+  local cursor = require 'feline.providers.cursor'
+  return ' ' .. cursor.line_percentage() .. ' '
+end
 
-basic.vi_mode = {
-    name = 'vi_mode',
-    hl_colors = colors_mode,
-    text = function()
-        return { { ' ', state.mode[2] } }
-    end,
-}
-basic.square_mode = {
-    hl_colors = colors_mode,
-    text = function()
-        return { { '▊', state.mode[2] } }
-    end,
-}
+local vi_mode_hl = function()
+  local vi_mode = require 'feline.providers.vi_mode'
+  return {
+    name = vi_mode.get_mode_highlight_name(),
+    fg = 'bg',
+    bg = vi_mode.get_mode_color(),
+    style = 'bold',
+  }
+end
 
-basic.lsp_diagnos = {
-    name = 'diagnostic',
-    hl_colors = {
-        red = { 'red', 'black' },
-        yellow = { 'yellow', 'black' },
-        blue = { 'blue', 'black' },
-    },
-    width = breakpoint_width,
-    text = function()
-        if lsp_comps.check_lsp() then
-            return {
-                { ' ', 'red' },
-                { lsp_comps.lsp_error({ format = ' %s', show_zero = true }), 'red' },
-                { lsp_comps.lsp_warning({ format = '  %s', show_zero = true }), 'yellow' },
-                { lsp_comps.lsp_hint({ format = '  %s', show_zero = true }), 'blue' },
-            }
-        end
-        return ''
-    end,
-}
-basic.file = {
-    name = 'file',
-    hl_colors = {
-        default = hl_list.Black,
-        white = { 'white', 'black' },
-        magenta = { 'magenta', 'black' },
-    },
-    text = function(_, winnr)
-        if vim.api.nvim_win_get_width(winnr) > breakpoint_width then
-            return {
-                { b_components.cache_file_size(), 'default' },
-                { ' ', '' },
-                { b_components.cache_file_name('[No Name]', ''), 'magenta' },
-                { b_components.line_col, 'white' },
-                { b_components.progress, '' },
-                { ' ', '' },
-                { b_components.file_modified(' '), 'magenta' },
-            }
-        else
-            return {
-                { b_components.cache_file_size(), 'default' },
-                { ' ', '' },
-                { b_components.cache_file_name('[No Name]', ''), 'magenta' },
-                { ' ', '' },
-                { b_components.file_modified(' '), 'magenta' },
-            }
-        end
-    end,
-}
-basic.file_right = {
-    hl_colors = {
-        default = hl_list.Black,
-        white = { 'white', 'black' },
-        magenta = { 'magenta', 'black' },
-    },
-    text = function(_, winnr)
-        if vim.api.nvim_win_get_width(winnr) < breakpoint_width then
-            return {
-                { b_components.line_col, 'white' },
-                { b_components.progress, '' },
-            }
-        end
-    end,
-}
-basic.git = {
-    name = 'git',
-    hl_colors = {
-        green = { 'green', 'black' },
-        red = { 'red', 'black' },
-        blue = { 'blue', 'black' },
-    },
-    width = breakpoint_width,
-    text = function()
-        if git_comps.is_git() then
-            return {
-                { ' ', '' },
-                { git_comps.diff_added({ format = ' %s', show_zero = true }), 'green' },
-                { git_comps.diff_removed({ format = '  %s', show_zero = true }), 'red' },
-                { git_comps.diff_changed({ format = ' 柳%s', show_zero = true }), 'blue' },
-            }
-        end
-        return ''
-    end,
-}
-
-basic.gps = {
-    function()
-        if gps.is_available() then
-            return gps.get_location()
-        end
-        return ''
-    end,
-    {"white", "black"}
-}
-
-local quickfix = {
-    filetypes = { 'qf', 'Trouble' },
+--[[ local gps = require("nvim-gps")
+local gps_provider = function()
+  return gps.get_location()
+end
+ ]]
+require'feline'.setup {
+  colors = {
+    black = '#434C5E',
+    skyblue = '#81A1C1',
+    cyan = '#88C0D0',
+    green  = '#8FBCBB',
+    oceanblue = '#5E81AC',
+    magenta = '#B48EAD',
+    orange = '#D08770',
+    red = '#EC5F67',
+    violet = '#B48EAD',
+    white  = '#ECEFF4',
+    yellow = '#EBCB8B',
+    fg = '#8FBCBB',
+    bg = '#2E3440',
+  },
+  vi_mode_colors = {
+    NORMAL = 'cyan',
+    OP = 'cyan',
+    INSERT = 'white',
+    VISUAL = 'green',
+    BLOCK = 'green',
+    REPLACE = 'yellow',
+    ['V-REPLACE'] = 'yellow',
+    ENTER = 'cyan',
+    MORE = 'cyan',
+    SELECT = 'magenta',
+    COMMAND = 'cyan',
+    SHELL = 'skyblue',
+    TERM = 'skyblue',
+    NONE = 'orange',
+  },
+  components = {
     active = {
-        { '🚦 Quickfix ', { 'white', 'black' } },
-        { helper.separators.slant_right, { 'black', 'black_light' } },
-        {
-            function()
-                return vim.fn.getqflist({ title = 0 }).title
-            end,
-            { 'cyan', 'black_light' },
-        },
-        { ' Total : %L ', { 'cyan', 'black_light' } },
-        { helper.separators.slant_right, { 'black_light', 'InactiveBg' } },
-        { ' ', { 'InactiveFg', 'InactiveBg' } },
-        basic.divider,
-        { helper.separators.slant_right, { 'InactiveBg', 'black' } },
-        { '🧛 ', { 'white', 'black' } },
-    },
-
-    always_active = true,
-}
-
-local explorer = {
-    filetypes = { 'fern', 'NvimTree', 'lir' },
-    active = {
-        { '  ', { 'white', 'black' } },
-        { helper.separators.slant_right, { 'black', 'black_light' } },
-        { b_components.divider, '' },
-        { b_components.file_name(''), { 'white', 'black_light' } },
-    },
-    always_active = true,
-    show_last_status = true,
-}
-local default = {
-    filetypes = { 'default' },
-    active = {
-        basic.square_mode,
-        basic.vi_mode,
-        basic.file,
-        basic.lsp_diagnos,
-        basic.divider,
-        basic.file_right,
-        { lsp_comps.lsp_name(), { 'magenta', 'black' }, breakpoint_width },
-        basic.git,
-        { git_comps.git_branch(), { 'magenta', 'black' }, breakpoint_width },
-        { ' ', hl_list.Black },
-        basic.square_mode,
+      {
+        { provider = vi_mode_provider, hl = vi_mode_hl, right_sep = ' ' },
+        { provider = 'git_branch' , icon = ' ', right_sep = '  ',
+          enabled = function() return vim.b.gitsigns_status_dict ~= nil end },
+        { provider = 'file_info' },
+        { provider = '' , hl = { fg = 'bg', bg = 'black' }},
+        -- { provider = gps_provider, enabled = gps.is_available() }
+      },
+      {},
+      {
+        { provider = function() return get_diag("Error") end,
+          hl = { fg = 'bg', bg = 'red', style = 'bold' },
+          left_sep = { str = '', hl = { fg = 'red', bg = 'black' }},
+          right_sep = { str = '', hl = { fg = 'yellow', bg = 'red' }}},
+        { provider = function() return get_diag("Warning") end,
+          hl = { fg = 'bg', bg = 'yellow', style = 'bold'  },
+          right_sep = { str = '', hl = { fg = 'cyan', bg = 'yellow' }}},
+        { provider = function() return get_diag("Information") end,
+          hl = { fg = 'bg', bg = 'cyan', style = 'bold' },
+          right_sep = { str = '', hl = { fg = 'oceanblue', bg = 'cyan' }}},
+        { provider = function() return get_diag("Hint") end,
+          hl = { fg = 'bg', bg = 'oceanblue', style = 'bold' },
+          right_sep = { str = '', hl = { fg = 'bg', bg = 'oceanblue', }}},
+        { provider = 'file_encoding', left_sep = ' ' },
+        { provider = 'position', left_sep = ' ', right_sep = ' ' },
+        { provider = percentage_provider,
+          hl = { fg = 'bg', bg = 'skyblue', style = 'bold' }},
+      }
     },
     inactive = {
-        { b_components.full_file_name, hl_list.Inactive },
-        basic.file_name_inactive,
-        basic.gps,
-        basic.divider,
-        basic.divider,
-        { b_components.line_col, hl_list.Inactive },
-        { b_components.progress, hl_list.Inactive },
+      {
+        { provider = vi_mode_provider, hl = vi_mode_hl, right_sep = ' ' },
+        { provider = 'git_branch' , icon = ' ', right_sep = '  ',
+          enabled = function() return vim.b.gitsigns_status_dict ~= nil end },
+        { provider = 'file_info' },
+        { provider = '' , hl = { fg = 'bg', bg = 'black' }},
+      },
+      {},
+      {}
     },
+  },
+  force_inactive = {
+    filetypes = {
+      'NvimTree',
+      'packer',
+      'LspTrouble',
+    },
+    buftypes = {'terminal'},
+    bufnames = {},
+  }
 }
-
-windline.setup({
-    statuslines = {
-        default,
-        quickfix,
-        explorer,
-    },
-})
