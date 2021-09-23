c6758a79f27056adf8b37de9c19073759489ce97
sternelee <sternelee@gmail.com>
2021-09-23 13:13:51 +0800

    use cmp

--- init.lua
+++ init.lua
@@ -78,8 +78,18 @@ require('packer').startup(function()
   } ]]
   -- 语法建议
   use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/nvim-compe'
-  use {'tzachar/compe-tabnine', run='./install.sh', requires = 'hrsh7th/nvim-compe'}
+  use {'hrsh7th/cmp-nvim-lsp', requires = {
+    {'hrsh7th/cmp-path'},
+    {'hrsh7th/cmp-buffer'},
+    {'hrsh7th/cmp-vsnip'},
+    {'ray-x/cmp-treesitter'},
+    {'hrsh7th/cmp-calc'},
+    {'hrsh7th/cmp-emoji'},
+    {'f3fora/cmp-spell'}
+    -- {'tzachar/cmp-tabnine', run='./install.sh'},
+  }}
+  use 'hrsh7th/nvim-cmp'
+  use { 'Saecki/crates.nvim', requires = { 'nvim-lua/plenary.nvim' } }
   -- 语法提示
   use 'folke/lsp-trouble.nvim'
   use 'glepnir/lspsaga.nvim'
@@ -401,6 +411,59 @@ require('nvim-treesitter.configs').setup {
   },
 }
 
+local lspkind = require('lspkind')
+local cmp = require'cmp'
+
+cmp.setup({
+  completion = {
+    autocomplete = true,
+    completeopt = 'menu,menuone,noinsert',
+  },
+  snippet = {
+    expand = function(args)
+      -- For `vsnip` user.
+      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` user.
+    end,
+  },
+  mapping = {
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' })
+  },
+  sources = {
+    { name = 'path' },
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' },
+    { name = 'buffer' },
+    { name = 'crates' },
+    { name = 'treesitter' },
+    { name = 'calc' },
+    { name = 'emoji' },
+    { name = 'spell' }
+    -- { name = "cmp_tabnine"},
+  },
+  formatting = {
+    format = function(entry, vim_item)
+      vim_item.kind = require("lspkind").presets.default[vim_item.kind] .. " " .. vim_item.kind
+      vim_item.menu = ({
+        path = "   [Path]",
+        buffer = "   [Buffer]",
+        nvim_lsp = "   [LSP]",
+        vsnip = "   [Vsnip]",
+        treesitter = "   [Ts]",
+        calc = "   [Calc]",
+        spell = "   [Spell]",
+        emoji = " ﲃ  [Emoji]",
+        tabnine = "⦿ [Tn]"
+      })[entry.source.name]
+      return vim_item
+    end
+  }
+})
+
 -- Signature help
 require('lsp_signature').on_attach()
 
@@ -462,16 +525,6 @@ local function documentHighlight(client, bufnr)
   end
 end
 
-local lsp_config = {}
-
-function lsp_config.common_on_attach(client, bufnr)
-  documentHighlight(client, bufnr)
-end
-function lsp_config.tsserver_on_attach(client, bufnr)
-  lsp_config.common_on_attach(client, bufnr)
-  client.resolved_capabilities.document_formatting = false
-end
-
 local on_attach = function(client, bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
   local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
@@ -518,18 +571,11 @@ local on_attach = function(client, bufnr)
 
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-capabilities.textDocument.completion.completionItem.resolveSupport = {
-  properties = {
-    'documentation',
-    'detail',
-    'additionalTextEdits',
-  }
-}
-
 -- npm install --global vls @volar/server vscode-langservers-extracted typescript typescript-language-server graphql-language-service-cli dockerfile-language-server-nodejs stylelint-lsp yaml-language-server diagnostic-languageserver eslint_d prettier
 -- can use rls or rust_analyzer
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 local function setup_servers()
   local servers = { "cssls", "html", "rust_analyzer", "tsserver",  "graphql", "vuels", "jsonls", "dockerls" }
@@ -537,7 +583,7 @@ local function setup_servers()
   for _, server in pairs(servers) do
     nvim_lsp[server].setup{
       on_attach = on_attach,
-      capabilities = capabilities,
+      capabilities = capabilities
     }
   end
   nvim_lsp.diagnosticls.setup {
@@ -601,93 +647,6 @@ end
 
 setup_servers()
 
---nvim-compe
-require'compe'.setup {
-  enabled = true,
-    autocomplete = true,
-    debug = false,
-    min_length = 1,
-    preselect = 'enable',
-    throttle_time = 80,
-    source_timeout = 200,
-    incomplete_delay = 400,
-    max_abbr_width = 100,
-    max_kind_width = 100,
-    max_menu_width = 100,
-    documentation = true,
-
-    source = {
-        path = {kind = "   (Path)"},
-        buffer = {kind = "   (Buffer)"},
-        calc = {kind = "   (Calc)"},
-        vsnip = {kind = "   (Snippet)"},
-        nvim_lsp = {kind = "   (LSP)"},
-        -- nvim_lua = {kind = "  "},
-		    nvim_lua = false,
-        spell = {kind = "   (Spell)"},
-        tags = false,
-        vim_dadbod_completion = true,
-        -- snippets_nvim = {kind = "  "},
-        -- ultisnips = {kind = "  "},
-        treesitter = {kind = "  "},
-        emoji = {kind = " ﲃ  (Emoji)", filetypes={"markdown", "text"}},
-        tabnine = {
-          max_line = 5000,
-          max_num_results = 6,
-          priority = 500,
-          sort = false,
-          show_prediction_strength = true,
-          ignore_pattern = '',
-          kind = "⦿ (TN)",
-        }
-    }
-}
-
---use tab to navigate autocomplete
-local t = function(str)
-  return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
-local check_back_space = function()
-    local col = vim.fn.col('.') - 1
-    if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-        return true
-    else
-        return false
-    end
-end
-
--- Use (s-)tab to:
---- move to prev/next item in completion menuone
---- jump to prev/next snippet's placeholder
-_G.tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t "<C-n>"
-  elseif vim.fn.call("vsnip#available", {1}) == 1 then
-    return t "<Plug>(vsnip-expand-or-jump)"
-  elseif check_back_space() then
-    return t "<Tab>"
-  else
-    return vim.fn['compe#complete']()
-  end
-end
-_G.s_tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t "<C-p>"
-  elseif vim.fn.call("vsnip#jumpable", {-1}) == 1 then
-    return t "<Plug>(vsnip-jump-prev)"
-  else
-    return t "<S-Tab>"
-  end
-end
-
-remap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
-remap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
-remap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-remap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-remap('i', '<cr>', 'compe#confirm("<cr>")', { expr = true })
-remap('i', '<c-space>', 'compe#complete()', { expr = true })
-
 vim.lsp.set_log_level("debug")
 require("trouble").setup {}
 require("lspkind").init()
@@ -697,10 +656,6 @@ require('symbols-outline').setup()
 require'diffview'.setup{}
 
 require('nvim-autopairs').setup()
-require('nvim-autopairs.completion.compe').setup({
-  map_cr = true,
-  map_complete = true
-})
 
 require("dapui").setup({
   icons = {
