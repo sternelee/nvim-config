410d7dd9b1eee76528a49605aa24ba31c59e9c50
sternelee <sternelee@gmail.com>
2021-09-23 10:46:00 +0800

    update

--- init.lua
+++ init.lua
@@ -528,17 +528,75 @@ capabilities.textDocument.completion.completionItem.resolveSupport = {
   }
 }
 
--- npm install --global vls @volar/server vscode-langservers-extracted typescript typescript-language-server graphql-language-service-cli dockerfile-language-server-nodejs stylelint-lsp yaml-language-server
+-- npm install --global vls @volar/server vscode-langservers-extracted typescript typescript-language-server graphql-language-service-cli dockerfile-language-server-nodejs stylelint-lsp yaml-language-server diagnostic-languageserver eslint_d prettier
 -- can use rls or rust_analyzer
 
 local function setup_servers()
   local servers = { "cssls", "html", "rust_analyzer", "tsserver",  "graphql", "vuels", "jsonls", "dockerls" }
+  local nvim_lsp = require'lspconfig'
   for _, server in pairs(servers) do
-    require'lspconfig'[server].setup{
+    nvim_lsp[server].setup{
       on_attach = on_attach,
       capabilities = capabilities,
     }
   end
+  nvim_lsp.diagnosticls.setup {
+    on_attach = on_attach,
+    filetypes = { 'javascript', 'javascriptreact', 'json', 'typescript', 'typescriptreact', 'css', 'less', 'scss', 'markdown', 'pandoc' },
+    init_options = {
+      linters = {
+        eslint = {
+          command = 'eslint_d',
+          rootPatterns = { '.git' },
+          debounce = 100,
+          args = { '--stdin', '--stdin-filename', '%filepath', '--format', 'json' },
+          sourceName = 'eslint_d',
+          parseJson = {
+            errorsRoot = '[0].messages',
+            line = 'line',
+            column = 'column',
+            endLine = 'endLine',
+            endColumn = 'endColumn',
+            message = '[eslint] ${message} [${ruleId}]',
+            security = 'severity'
+          },
+          securities = {
+            [2] = 'error',
+            [1] = 'warning'
+          }
+        },
+      },
+      filetypes = {
+        javascript = 'eslint',
+        javascriptreact = 'eslint',
+        typescript = 'eslint',
+        typescriptreact = 'eslint',
+      },
+      formatters = {
+        eslint_d = {
+          command = 'eslint_d',
+          args = { '--stdin', '--stdin-filename', '%filename', '--fix-to-stdout' },
+          rootPatterns = { '.git' },
+        },
+        prettier = {
+          command = 'prettier',
+          args = { '--stdin-filepath', '%filename' }
+        }
+      },
+      formatFiletypes = {
+        css = 'prettier',
+        javascript = 'eslint_d',
+        javascriptreact = 'eslint_d',
+        json = 'prettier',
+        scss = 'prettier',
+        less = 'prettier',
+        typescript = 'eslint_d',
+        typescriptreact = 'eslint_d',
+        json = 'prettier',
+        markdown = 'prettier',
+      }
+  }
+}
 end
 
 setup_servers()
