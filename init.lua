e50e445a5189ed97894e243b071d17a945b24800
sternelee <sternelee@gmail.com>
2021-10-13 11:00:53 +0800

    update server

--- init.lua
+++ init.lua
@@ -525,7 +525,7 @@ vim.g.coq_settings = {
 }
 
 local function setup_servers()
-  local servers = { "cssls", "html", "rust_analyzer", "tsserver",  "graphql", "vuels", "jsonls", "dockerls" }
+  local servers = { "cssls", "html", "rust_analyzer", "tsserver",  "graphql", "volar", "jsonls", "dockerls" }
   local nvim_lsp = require'lspconfig'
   local coq = require "coq"
   local config = {
