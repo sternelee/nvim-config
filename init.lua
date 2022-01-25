2c163998f8253d6305eefbc3b746887223fff994
李兴广 <lixingguang@xunlei.com>
2022-01-25 13:37:13 +0800

    review

--- init.lua
+++ init.lua
@@ -273,12 +273,6 @@ cmd [[autocmd BufWritePre * %s/\n\+\%$//e]]
 cmd [[autocmd CursorHold,CursorHoldI * :lua require'nvim-lightbulb'.update_lightbulb()]]
 cmd [[autocmd FileChangedShellPost * :lua require'notify'("File changed on disk. Buffer reloaded!", 'warn', {'title': 'File Notify', timeout: '400'})]]
 
-cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
-cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
-cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
-cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
-cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
-cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
 cmd [[autocmd CursorHold <buffer> lua vim.lsp.buf.hover()]]
 
 -- https://github-wiki-see.page/m/neovim/nvim-lspconfig/wiki/UI-customization
@@ -327,18 +321,12 @@ let bufferline.icons = 'both'
 
 g.vista_default_executive = 'nvim_lsp'
 
+vim.opt.list = true
+vim.opt.listchars:append("space:⋅")
 require("indent_blankline").setup {
     space_char_blankline = " ",
     show_current_context = true,
-    show_current_context_start = true,
-    char_highlight_list = {
-        "IndentBlanklineIndent1",
-        "IndentBlanklineIndent2",
-        "IndentBlanklineIndent3",
-        "IndentBlanklineIndent4",
-        "IndentBlanklineIndent5",
-        "IndentBlanklineIndent6",
-    },
+    show_current_context_start = true
 }
 
 --theme
