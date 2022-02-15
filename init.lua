a1a7dece05a9b66157f981b4a82d42a5ecc1ac53
李兴广 <lixingguang@xunlei.com>
2022-02-15 10:31:19 +0800

    review

--- init.lua
+++ init.lua
@@ -350,67 +350,67 @@ require("indent_blankline").setup {
 }
 
 --theme
--- cmd 'colorscheme nightfly'
+cmd 'colorscheme nightfly'
 
-local catppuccin = require("catppuccin")
-catppuccin.setup{
-  transparent_background = false,
-  term_colors = false,
-  styles = {
-  	comments = "italic",
-  	functions = "NONE",
-  	keywords = "italic",
-  	strings = "NONE",
-  	variables = "NONE",
-  },
-  integrations = {
-  	treesitter = false,
-  	native_lsp = {
-  		enabled = true,
-  		virtual_text = {
-  			errors = "italic",
-  			hints = "italic",
-  			warnings = "italic",
-  			information = "italic",
-  		},
-  		underlines = {
-  			errors = "underline",
-  			hints = "underline",
-  			warnings = "underline",
-  			information = "underline",
-  		},
-  	},
-  	lsp_trouble = true,
-  	cmp = true,
-  	lsp_saga = true,
-  	gitgutter = false,
-  	gitsigns = true,
-  	telescope = true,
-  	nvimtree = {
-  		enabled = true,
-  		show_root = false,
-  		transparent_panel = false,
-  	},
-  	which_key = true,
-  	indent_blankline = {
-  		enabled = true,
-  		colored_indent_levels = false,
-  	},
-  	dashboard = false,
-  	neogit = false,
-  	vim_sneak = false,
-  	fern = false,
-  	barbar = true,
-  	bufferline = true,
-  	markdown = true,
-  	lightspeed = true,
-  	ts_rainbow = true,
-  	hop = true,
-  	notify = true,
-  	telekasten = true,
-  }
-}
-cmd 'colorscheme catppuccin'
+-- local catppuccin = require("catppuccin")
+-- catppuccin.setup{
+--   transparent_background = false,
+--   term_colors = false,
+--   styles = {
+--   	comments = "italic",
+--   	functions = "NONE",
+--   	keywords = "italic",
+--   	strings = "NONE",
+--   	variables = "NONE",
+--   },
+--   integrations = {
+--   	treesitter = false,
+--   	native_lsp = {
+--   		enabled = true,
+--   		virtual_text = {
+--   			errors = "italic",
+--   			hints = "italic",
+--   			warnings = "italic",
+--   			information = "italic",
+--   		},
+--   		underlines = {
+--   			errors = "underline",
+--   			hints = "underline",
+--   			warnings = "underline",
+--   			information = "underline",
+--   		},
+--   	},
+--   	lsp_trouble = true,
+--   	cmp = true,
+--   	lsp_saga = true,
+--   	gitgutter = false,
+--   	gitsigns = true,
+--   	telescope = true,
+--   	nvimtree = {
+--   		enabled = true,
+--   		show_root = false,
+--   		transparent_panel = false,
+--   	},
+--   	which_key = true,
+--   	indent_blankline = {
+--   		enabled = true,
+--   		colored_indent_levels = false,
+--   	},
+--   	dashboard = false,
+--   	neogit = false,
+--   	vim_sneak = false,
+--   	fern = false,
+--   	barbar = true,
+--   	bufferline = true,
+--   	markdown = true,
+--   	lightspeed = true,
+--   	ts_rainbow = true,
+--   	hop = true,
+--   	notify = true,
+--   	telekasten = true,
+--   }
+-- }
+-- cmd 'colorscheme catppuccin'
 
 -- g.sonokai_style = 'andromeda'
 
