da564048001acb01b5cc9397c2aaef04472c8380
李兴广 <lixingguang@xunlei.com>
2022-02-21 13:34:56 +0800

    review

--- init.lua
+++ init.lua
@@ -190,6 +190,21 @@ require('packer').startup(function()
   }
   -- use 'nanotee/sqls.nvim'
   -- use 'KenN7/vim-arsync'
+  use {
+    'rmagatti/auto-session',
+    config = function()
+      require('auto-session').setup {
+        log_level = 'info',
+        auto_session_enable_last_session = false,
+        auto_session_root_dir = vim.fn.stdpath('data').."/sessions/",
+        auto_session_enabled = true,
+        auto_save_enabled = nil,
+        auto_restore_enabled = nil,
+        auto_session_suppress_dirs = nil,
+        bypass_session_save_file_types = nil
+      }
+    end
+  }
 
 end)
 
@@ -255,6 +270,8 @@ opt('o', 'showtabline', 2)
 
 --set shortmess
 vim.o.shortmess = vim.o.shortmess .. "c"
+
+vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages"
 
 nvim_exec([[
 filetype on
