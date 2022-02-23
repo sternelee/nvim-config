07c6fe5beca94c2666625ad5d4791d0212b85b16
李兴广 <lixingguang@xunlei.com>
2022-02-23 13:22:41 +0800

    review

--- init.lua
+++ init.lua
@@ -59,10 +59,6 @@ require('packer').startup(function()
   -- use 'sainnhe/sonokai'
   use 'bluz71/vim-nightfly-guicolors'
   -- use 'Iron-E/nvim-highlite'
-  use({
-  	"catppuccin/nvim",
-  	as = "catppuccin"
-  })
   -- 显示导航线
   use {'lukas-reineke/indent-blankline.nvim', event = 'BufRead',
     config = function() -- 导航finder操作
@@ -412,68 +408,7 @@ vim.opt.listchars:append("space:⋅")
 
 
 --theme
--- cmd 'colorscheme nightfly'
-
-local catppuccin = require("catppuccin")
-catppuccin.setup{
-  transparent_background = false,
-  term_colors = false,
-  styles = {
-  	comments = "italic",
-  	functions = "NONE",
-  	keywords = "italic",
-  	strings = "NONE",
-  	variables = "NONE",
-  },
-  integrations = {
-  	treesitter = false,
-  	native_lsp = {
-  		enabled = true,
-  		virtual_text = {
-  			errors = "italic",
-  			hints = "italic",
-  			warnings = "italic",
-  			information = "italic",
-  		},
-  		underlines = {
-  			errors = "underline",
-  			hints = "underline",
-  			warnings = "underline",
-  			information = "underline",
-  		},
-  	},
-  	lsp_trouble = true,
-  	cmp = true,
-  	lsp_saga = true,
-  	gitgutter = false,
-  	gitsigns = true,
-  	telescope = true,
-  	nvimtree = {
-  		enabled = true,
-  		show_root = false,
-  		transparent_panel = false,
-  	},
-  	which_key = true,
-  	indent_blankline = {
-  		enabled = true,
-  		colored_indent_levels = false,
-  	},
-  	dashboard = false,
-  	neogit = false,
-  	vim_sneak = false,
-  	fern = false,
-  	barbar = true,
-  	bufferline = true,
-  	markdown = true,
-  	lightspeed = false,
-  	ts_rainbow = true,
-  	hop = true,
-  	notify = true,
-  	telekasten = true,
-  }
-}
-
-cmd 'colorscheme catppuccin'
+cmd 'colorscheme nightfly'
 
 -- g.sonokai_style = 'andromeda'
 
