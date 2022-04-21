b79c36c3d64003c013722e0da519cf826183135a
李兴广 <lixingguang@xunlei.com>
2022-04-21 11:06:45 +0800

    save

--- init.lua
+++ init.lua
@@ -14,7 +14,7 @@ g.loaded_ruby_provider = 0
 g.loaded_perl_provider = 0
 
 -- g.neovide_transparency=0.96
--- g.neovide_cursor_vfx_mode = "sonicboom"
+g.neovide_cursor_vfx_mode = "sonicboom"
 
 nvim_exec([[set guifont=VictorMono\ NF:h18]], false)
 
@@ -54,6 +54,11 @@ packer.startup({function()
   use {'f-person/git-blame.nvim', event = 'BufRead'}-- 显示git message
   use {'rbong/vim-flog', opt = true, cmd = {'Flog'}}
   use {'junegunn/gv.vim', opt = true, cmd = {'GV'}}
+  use {'sindrets/diffview.nvim', opt = true, cmd = {'DiffviewOpen', 'DiffviewToggleFiles', 'DiffviewFocusFiles'},
+    config = function ()
+      require('diffview').setup()
+    end
+  }
   -- 语法高亮
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate', lock = true }
   use {'nvim-treesitter/nvim-treesitter-refactor', opt = true, event = 'InsertEnter', config = function() require('nvim-treesitter-refactor').init() end}
@@ -478,6 +483,7 @@ map('n', '<leader>tr', '<cmd>NvimTreeRefresh<CR>')
 -- map('n', '<leader>tb', '<cmd>SidebarNvimToggle<CR>')
 map('n', '<leader>tl', '<cmd>Twilight<CR>')
 map('n', '<leader>tw', '<cmd>Translate<CR>')
+-- map('n', '<leader>th', '<cmd>lua require("hlargs").toggle()<CR>')
 -- nvim-lsp-ts-utils
 map('n', '<leader>to', '<cmd>TSLspOrganize<CR>')
 map('n', '<leader>tn', '<cmd>TSLspRenameFile<CR>')
@@ -663,126 +669,7 @@ vim.lsp.handlers['window/showMessage'] = function(_, result, ctx)
   })
 end
 
-local actions = require('telescope.actions')
-local action_layout = require('telescope.actions.layout')
-require('telescope').setup {
-  defaults = {
-    vimgrep_arguments = {
-      'rg',
-      '--color=never',
-      '--no-heading',
-      '--with-filename',
-      '--line-number',
-      '--column',
-      '--smart-case',
-    },
-    prompt_prefix = ' ',
-    selection_caret = '  ',
-    entry_prefix = '  ',
-    initial_mode = 'insert',
-    selection_strategy = 'reset',
-    sorting_strategy = 'ascending',
-    layout_strategy = 'horizontal',
-    layout_config = {
-      horizontal = {
-        prompt_position = 'top',
-        preview_width = 0.55,
-        results_width = 0.8,
-      },
-      vertical = {
-        mirror = false,
-      },
-      width = 0.87,
-      height = 0.80,
-      preview_cutoff = 120,
-    },
-    file_sorter = require('telescope.sorters').get_fuzzy_file,
-    file_ignore_patterns = { 'node_modules' },
-    generic_sorter = require('telescope.sorters').get_generic_fuzzy_sorter,
-    path_display = { 'truncate' },
-    winblend = 0,
-    border = {},
-    borderchars = { '─', '│', '─', '│', '┌', '┐', '┘', '└' },
-    color_devicons = true,
-    use_less = true,
-    set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
-    file_previewer = require('telescope.previewers').vim_buffer_cat.new,
-    grep_previewer = require('telescope.previewers').vim_buffer_vimgrep.new,
-    qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
-    -- Developer configurations: Not meant for general override
-    buffer_previewer_maker = require('telescope.previewers').buffer_previewer_maker,
-
-    mappings = {
-      i = {
-        ['<C-n>'] = actions.cycle_history_next,
-        ['<C-p>'] = actions.cycle_history_prev,
-        ['<M-p>'] = action_layout.toggle_preview,
-
-        ['<C-j>'] = actions.move_selection_next,
-        ['<C-k>'] = actions.move_selection_previous,
-
-        ['<esc>'] = actions.close,
-        ['<C-[>'] = actions.close,
-
-        ['<CR>'] = actions.select_default,
-        ['<C-x>'] = actions.select_horizontal,
-        ['<C-v>'] = actions.select_vertical,
-        ['<C-t>'] = actions.select_tab,
-
-        ['<C-u>'] = actions.preview_scrolling_up,
-        ['<C-d>'] = actions.preview_scrolling_down,
-
-        ['<Tab>'] = actions.toggle_selection + actions.move_selection_worse,
-        ['<S-Tab>'] = actions.toggle_selection + actions.move_selection_better,
-        ['<C-q>'] = actions.send_to_qflist + actions.open_qflist,
-        ['<M-q>'] = actions.send_selected_to_qflist + actions.open_qflist,
-        ['<C-l>'] = actions.complete_tag,
-        ['<C-_>'] = actions.which_key, -- keys from pressing <C-/>
-      },
-
-      n = {
-        ['<M-p>'] = action_layout.toggle_preview,
-
-        ['<esc>'] = actions.close,
-        ['<C-[>'] = actions.close,
-
-        ['<CR>'] = actions.select_default,
-        ['<C-x>'] = actions.select_horizontal,
-        ['<C-v>'] = actions.select_vertical,
-        ['<C-t>'] = actions.select_tab,
-
-        ['<Tab>'] = actions.toggle_selection + actions.move_selection_worse,
-        ['<S-Tab>'] = actions.toggle_selection + actions.move_selection_better,
-        ['<C-q>'] = actions.send_to_qflist + actions.open_qflist,
-        ['<M-q>'] = actions.send_selected_to_qflist + actions.open_qflist,
-
-        ['j'] = actions.move_selection_next,
-        ['k'] = actions.move_selection_previous,
-        ['H'] = actions.move_to_top,
-        ['M'] = actions.move_to_middle,
-        ['L'] = actions.move_to_bottom,
-
-        ['gg'] = actions.move_to_top,
-        ['G'] = actions.move_to_bottom,
-
-        ['<C-u>'] = actions.preview_scrolling_up,
-        ['<C-d>'] = actions.preview_scrolling_down,
-
-        ['?'] = actions.which_key,
-      },
-    },
-  },
-  pickers = {
-    find_files = {
-      previewer = false,
-    },
-  },
-}
-
-require'telescope'.load_extension('file_browser')
-require'telescope'.load_extension('notify')
-require'telescope'.load_extension('packer')
-require'telescope'.load_extension('projects')
+require('telescope_config')
 
 local disableTS = function (lang, bufnr)
   return vim.api.nvim_buf_line_count(bufnr) > 10000
@@ -1014,19 +901,16 @@ local on_attach = function(client, bufnr)
     ts_utils.setup_client(client)
   end
 
-  if client.name == 'tailwindcss' then
-  end
-
-  if client.name ~= 'jsonls' then
-    local msg = string.format("Language server %s started!", client.name)
-    notify(msg, 'info', {title = 'LSP Notify', timeout = '300'})
-    -- require'lsp_signature'.on_attach({
-    --   bind = true,
-    --   handler_opts = {
-    --     border = "rounded"
-    --   }
-    -- }, bufnr)
-  end
+  -- if client.name ~= 'jsonls' then
+  --   local msg = string.format("Language server %s started!", client.name)
+  --   notify(msg, 'info', {title = 'LSP Notify', timeout = '300'})
+  --   -- require'lsp_signature'.on_attach({
+  --   --   bind = true,
+  --   --   handler_opts = {
+  --   --     border = "rounded"
+  --   --   }
+  --   -- }, bufnr)
+  -- end
 
 end
 
@@ -1335,313 +1219,8 @@ require'Comment'.setup {
   end,
 }
 
--- windline config
-local windline = require('windline')
-local helper = require('windline.helpers')
-local sep = helper.separators
-local b_components = require('windline.components.basic')
-local state = _G.WindLine.state
-local vim_components = require('windline.components.vim')
-local HSL = require('wlanimation.utils')
+require'statusline'
 
-local lsp_comps = require('windline.components.lsp')
-local git_comps = require('windline.components.git')
-
-local gps = require("nvim-gps")
-gps.setup()
-local hl_list = {
-    Black = { 'white', 'black' },
-    White = { 'black', 'white' },
-    Normal = { 'NormalFg', 'NormalBg' },
-    Inactive = { 'InactiveFg', 'InactiveBg' },
-    Active = { 'ActiveFg', 'ActiveBg' },
-}
-local basic = {}
-
-local airline_colors = {}
-
-basic.gps = {
-  function()
-    if gps.is_available() then
-      return ' '..gps.get_location()
-    end
-    return ''
-  end,
-  {"cyan", "NormalBg"}
-}
-
-airline_colors.a = {
-    NormalSep = { 'magenta_a', 'magenta_b' },
-    InsertSep = { 'green_a', 'green_b' },
-    VisualSep = { 'yellow_a', 'yellow_b' },
-    ReplaceSep = { 'blue_a', 'blue_b' },
-    CommandSep = { 'red_a', 'red_b' },
-    Normal = { 'black', 'magenta_a' },
-    Insert = { 'black', 'green_a' },
-    Visual = { 'black', 'yellow_a' },
-    Replace = { 'black', 'blue_a' },
-    Command = { 'black', 'red_a' },
-}
-
-airline_colors.b = {
-    NormalSep = { 'magenta_b', 'magenta_c' },
-    InsertSep = { 'green_b', 'green_c' },
-    VisualSep = { 'yellow_b', 'yellow_c' },
-    ReplaceSep = { 'blue_b', 'blue_c' },
-    CommandSep = { 'red_b', 'red_c' },
-    Normal = { 'white', 'magenta_b' },
-    Insert = { 'white', 'green_b' },
-    Visual = { 'white', 'yellow_b' },
-    Replace = { 'white', 'blue_b' },
-    Command = { 'white', 'red_b' },
-}
-
-airline_colors.c = {
-    NormalSep = { 'magenta_c', 'NormalBg' },
-    InsertSep = { 'green_c', 'NormalBg' },
-    VisualSep = { 'yellow_c', 'NormalBg' },
-    ReplaceSep = { 'blue_c', 'NormalBg' },
-    CommandSep = { 'red_c', 'NormalBg' },
-    Normal = { 'white', 'magenta_c' },
-    Insert = { 'white', 'green_c' },
-    Visual = { 'white', 'yellow_c' },
-    Replace = { 'white', 'blue_c' },
-    Command = { 'white', 'red_c' },
-}
-
-basic.divider = { b_components.divider, hl_list.Normal }
-
-local width_breakpoint = 100
-
-basic.section_a = {
-    hl_colors = airline_colors.a,
-    text = function(_,_,width)
-        if width > width_breakpoint then
-            return {
-                { ' ' .. state.mode[1] .. ' ', state.mode[2] },
-                { sep.right_filled, state.mode[2] .. 'Sep' },
-            }
-        end
-        return {
-            { ' ' .. state.mode[1]:sub(1, 1) .. ' ', state.mode[2] },
-            { sep.right_filled, state.mode[2] .. 'Sep' },
-        }
-    end,
-}
-
-
-basic.section_b = {
-    hl_colors = airline_colors.b,
-    text = function(bufnr,_, width)
-        if width > width_breakpoint and git_comps.is_git(bufnr) then
-            return {
-                { git_comps.git_branch() , state.mode[2] },
-                { ' ', '' },
-                { sep.right_filled, state.mode[2] .. 'Sep' },
-            }
-        end
-        return { { sep.right_filled, state.mode[2] .. 'Sep' } }
-    end,
-}
-
-
-basic.section_c = {
-    hl_colors = airline_colors.c,
-    text = function()
-        return {
-            { ' ', state.mode[2] },
-            { b_components.cache_file_name('[No Name]', 'unique')},
-            { ' '},
-            { sep.right_filled, state.mode[2] .. 'Sep' },
-        }
-    end,
-}
-
-basic.section_x = {
-    hl_colors = airline_colors.c,
-    text = function(_,_,width)
-        if width > width_breakpoint then
-            return {
-            { sep.left_filled, state.mode[2] .. 'Sep' },
-            { ' ', state.mode[2] },
-            { b_components.file_encoding()},
-            { ' ' },
-            { b_components.file_format({ icon = true }) },
-            { ' ' },
-            }
-        end
-        return {
-            { sep.left_filled, state.mode[2] .. 'Sep' },
-        }
-    end,
-}
-
-basic.section_y = {
-    hl_colors = airline_colors.b,
-    text = function(_,_,width)
-        if width > width_breakpoint then
-            return {
-                { sep.left_filled, state.mode[2] .. 'Sep' },
-                { b_components.cache_file_type({ icon = true }), state.mode[2] },
-                { ' ' },
-            }
-        end
-        return { { sep.left_filled, state.mode[2] .. 'Sep' } }
-    end,
-}
-
-basic.section_z = {
-    hl_colors = airline_colors.a,
-    text = function(_,_,width)
-        if width > width_breakpoint then
-            return {
-                { sep.left_filled, state.mode[2] .. 'Sep' },
-                { '', state.mode[2] },
-                { b_components.progress_lua},
-                { ' '},
-                { b_components.line_col_lua},
-            }
-        end
-        return {
-            { sep.left_filled, state.mode[2] .. 'Sep' },
-            { ' ', state.mode[2] },
-            { b_components.line_col_lua, state.mode[2] },
-        }
-    end,
-}
-
-basic.lsp_diagnos = {
-    name = 'diagnostic',
-    hl_colors = {
-        red = { 'red', 'NormalBg' },
-        yellow = { 'yellow', 'NormalBg' },
-        blue = { 'blue', 'NormalBg' },
-    },
-    text = function(bufnr)
-        if lsp_comps.check_lsp(bufnr) then
-            return {
-                { lsp_comps.lsp_error({ format = '  %s', show_zero = true }), 'red' },
-                { lsp_comps.lsp_warning({ format = '  %s', show_zero = true }), 'yellow' },
-                { lsp_comps.lsp_hint({ format = '  %s', show_zero = true }), 'blue' },
-            }
-        end
-        return { ' ', 'red' }
-    end,
-}
-
-basic.git = {
-    name = 'git',
-    width = width_breakpoint,
-    hl_colors = {
-        green = { 'green', 'NormalBg' },
-        red = { 'red', 'NormalBg' },
-        blue = { 'blue', 'NormalBg' },
-    },
-    text = function(bufnr)
-        if git_comps.is_git(bufnr) then
-            return {
-                { git_comps.diff_added({ format = '  %s' }), 'green' },
-                { git_comps.diff_removed({ format = '  %s' }), 'red' },
-                { git_comps.diff_changed({ format = '  %s' }), 'blue' },
-            }
-        end
-        return ''
-    end,
-}
-local quickfix = {
-    filetypes = { 'qf', 'Trouble' },
-    active = {
-        { '🚦 Quickfix ', { 'white', 'black' } },
-        { helper.separators.slant_right, { 'black', 'black_light' } },
-        {
-            function()
-                return vim.fn.getqflist({ title = 0 }).title
-            end,
-            { 'cyan', 'black_light' },
-        },
-        { ' Total : %L ', { 'cyan', 'black_light' } },
-        { helper.separators.slant_right, { 'black_light', 'InactiveBg' } },
-        { ' ', { 'InactiveFg', 'InactiveBg' } },
-        basic.divider,
-        { helper.separators.slant_right, { 'InactiveBg', 'black' } },
-        { '🧛 ', { 'white', 'black' } },
-    },
-    always_active = true,
-    show_last_status = true
-}
-
-local explorer = {
-    filetypes = { 'fern', 'NvimTree', 'lir' },
-    active = {
-        { '  ', { 'white', 'magenta_b' } },
-        { helper.separators.slant_right, { 'magenta_b', 'NormalBg' } },
-        { b_components.divider, '' },
-        { b_components.file_name(''), { 'NormalFg', 'NormalBg' } },
-    },
-    always_active = true,
-    show_last_status = true
-}
-
-local default = {
-    filetypes = { 'default' },
-    active = {
-        basic.section_a,
-        basic.section_b,
-        basic.section_c,
-        basic.lsp_diagnos,
-        { vim_components.search_count(), { 'cyan', 'NormalBg' } },
-        basic.gps,
-        basic.divider,
-        basic.git,
-        basic.section_x,
-        basic.section_y,
-        basic.section_z,
-    },
-    inactive = {
-        { b_components.full_file_name, hl_list.Inactive },
-        { b_components.divider, hl_list.Inactive },
-        { b_components.line_col, hl_list.Inactive },
-        { b_components.progress, hl_list.Inactive },
-    },
-}
-
-windline.setup({
-    colors_name = function(colors)
-        local mod = function (c, value)
-            if vim.o.background == 'light' then
-                return HSL.rgb_to_hsl(c):tint(value):to_rgb()
-            end
-            return HSL.rgb_to_hsl(c):shade(value):to_rgb()
-        end
-
-        colors.magenta_a = colors.magenta
-        colors.magenta_b = mod(colors.magenta,0.5)
-        colors.magenta_c = mod(colors.magenta,0.7)
-
-        colors.yellow_a = colors.yellow
-        colors.yellow_b = mod(colors.yellow,0.5)
-        colors.yellow_c = mod(colors.yellow,0.7)
-
-        colors.blue_a = colors.blue
-        colors.blue_b = mod(colors.blue,0.5)
-        colors.blue_c = mod(colors.blue,0.7)
-
-        colors.green_a = colors.green
-        colors.green_b = mod(colors.green,0.5)
-        colors.green_c = mod(colors.green,0.7)
-
-        colors.red_a = colors.red
-        colors.red_b = mod(colors.red,0.5)
-        colors.red_c = mod(colors.red,0.7)
-
-        return colors
-    end,
-    statuslines = {
-        default,
-        quickfix,
-        explorer,
-    },
-})
 -- telekasten
 local home = vim.fn.expand("~/zettelkasten")
 require('telekasten').setup({
