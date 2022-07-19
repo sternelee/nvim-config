3c844ae8fe100f354750647d4979d461e8be2b64
李兴广 <lixingguang@xunlei.com>
2022-07-19 23:09:21 +0800

    feat: lualine add gps

--- lua/modules/lualine.lua
+++ lua/modules/lualine.lua
@@ -1,4 +1,14 @@
 -- lualine config
+local gps = require("nvim-gps")
+gps.setup()
+
+local gps_c = function()
+  if gps.is_available() then
+    return ' '..gps.get_location()
+  end
+  return ''
+end
+
 require('lualine').setup({
   options = {
     icons_enabled = true,
@@ -12,7 +22,7 @@ require('lualine').setup({
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
+    lualine_c = {'filename', gps_c},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
@@ -20,7 +30,7 @@ require('lualine').setup({
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
+    lualine_c = {'filename', gps_c},
     lualine_x = {'location'},
     lualine_y = {},
     lualine_z = {}
