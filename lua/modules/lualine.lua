146737aef53bc29fc6db896b5c6ce531f2fd1e67
李兴广 <lixingguang@xunlei.com>
2022-07-19 23:16:19 +0800

    review

--- lua/modules/lualine.lua
+++ lua/modules/lualine.lua
@@ -12,7 +12,7 @@ end
 require('lualine').setup({
   options = {
     icons_enabled = true,
-    theme = 'nightfly',
+    theme = 'horizon',
     component_separators = { left = '', right = ''},
     section_separators = { left = '', right = ''},
     disabled_filetypes = {},
