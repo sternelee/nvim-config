603275502bab5ec8137b721d0e74a28c1b3f674a
李兴广 <lixingguang@xunlei.com>
2022-07-28 10:06:46 +0800

    review

--- init.lua
+++ init.lua
@@ -661,22 +661,22 @@ g.EditorConfig_exclude_patterns = {'fugitive://.*', 'scp://.*', ''}
 local notify = require("notify")
 vim.notify = notify
 
-vim.lsp.handlers['window/showMessage'] = function(_, result, ctx)
-  local client = vim.lsp.get_client_by_id(ctx.client_id)
-  local lvl = ({
-    'ERROR',
-    'WARN',
-    'INFO',
-    'DEBUG',
-  })[result.type]
-  notify({ result.message }, lvl, {
-    title = 'LSP | ' .. client.name,
-    timeout = 10000,
-    keep = function()
-      return lvl == 'ERROR' or lvl == 'WARN'
-    end,
-  })
-end
+-- vim.lsp.handlers['window/showMessage'] = function(_, result, ctx)
+--   local client = vim.lsp.get_client_by_id(ctx.client_id)
+--   local lvl = ({
+--     'ERROR',
+--     'WARN',
+--     'INFO',
+--     'DEBUG',
+--   })[result.type]
+--   notify({ result.message }, lvl, {
+--     title = 'LSP | ' .. client.name,
+--     timeout = 10000,
+--     keep = function()
+--       return lvl == 'ERROR' or lvl == 'WARN'
+--     end,
+--   })
+-- end
 
 require'modules.telescope'
 
@@ -933,10 +933,10 @@ local on_attach = function(client, bufnr)
     end
   end
 
-  if client.name ~= 'jsonls' then
-    local msg = string.format("Language server %s started!", client.name)
-    notify(msg, 'info', {title = 'LSP Notify', timeout = '100'})
-  end
+  -- if client.name ~= 'jsonls' then
+  --   local msg = string.format("Language server %s started!", client.name)
+  --   notify(msg, 'info', {title = 'LSP Notify', timeout = '100'})
+  -- end
 
 end
 
