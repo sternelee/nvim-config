9d82b3dbd56534303ae8d1b7762befba2798fec3
李兴广 <lixingguang@xunlei.com>
2022-09-25 22:03:30 +0800

    review

--- lua/modules/windline.lua
+++ lua/modules/windline.lua
@@ -31,7 +31,7 @@ basic.vi_mode = {
     name = 'vi_mode',
     hl_colors = colors_mode,
     text = function()
-        return { { '☉', state.mode[2] } }
+        return { { '☉ ', state.mode[2] } }
     end,
 }
 basic.square_mode = {
