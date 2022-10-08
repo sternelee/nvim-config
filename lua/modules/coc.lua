73fa6c7b187d45b7003ddc486ef02719f763a3e5
李兴广 <lixingguang@xunlei.com>
2022-10-08 16:46:18 +0800

    review

--- lua/modules/coc.lua
+++ lua/modules/coc.lua
@@ -12,19 +12,13 @@ local opts = {silent = true, noremap = true, expr = true}
 remap("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
 remap("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
 
--- Make <CR> to accept selected completion item or notify coc.nvim to format
--- <C-g>u breaks current undo, please make your own choice.
 remap("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
 
--- Use <c-j> to trigger snippets
 keyset("i", "<c-j>", "<Plug>(coc-snippets-expand-jump)")
--- Use <c-space> to trigger completion.
 keyset("i", "<c-space>", "coc#refresh()", {silent = true, expr = true})
 
--- Use `[g` and `]g` to navigate diagnostics
--- Use `:CocDiagnostics` to get all diagnostics of current buffer in location list.
-keyset("n", "[g", "<Plug>(coc-diagnostic-prev)", {silent = true})
-keyset("n", "]g", "<Plug>(coc-diagnostic-next)", {silent = true})
+keyset("n", "[d", "<Plug>(coc-diagnostic-prev)", {silent = true})
+keyset("n", "]d", "<Plug>(coc-diagnostic-next)", {silent = true})
 
 -- GoTo code navigation.
 keyset("n", "gd", "<Plug>(coc-definition)", {silent = true})
