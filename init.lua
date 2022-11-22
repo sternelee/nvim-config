d4f43232158f0dd504d9935fce7b1f05c260873b
李兴广 <lixingguang@xunlei.com>
2022-11-22 17:27:14 +0800

    feat: use wilder.nvim

--- init.lua
+++ init.lua
@@ -121,6 +121,22 @@ packer.startup({function()
   use {'numToStr/FTerm.nvim', opt = true, event = 'BufRead'}
   use {'is0n/fm-nvim', opt = true, event = 'BufRead'}
   use {"petertriho/nvim-scrollbar", config = function()require("scrollbar").setup() end}
+  use {
+    'gelguy/wilder.nvim',
+    config = function()
+      local wilder = require('wilder')
+      wilder.set_option('renderer', wilder.popupmenu_renderer(
+        wilder.popupmenu_palette_theme({
+          border = 'rounded',
+          max_height = '75%',      -- max height of the palette
+          min_height = 0,          -- set to the same as 'max_height' for a fixed height window
+          prompt_position = 'top', -- 'top' or 'bottom' to set the location of the prompt
+          reverse = 0,             -- set to 1 to reverse the order of the list, use in combination with 'prompt_position'
+        })
+      ))
+      wilder.setup({modes = {':', '/', '?'}})
+    end,
+  }
   -- use({
   --   "folke/noice.nvim",
   --   event = "VimEnter",
