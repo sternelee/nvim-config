4114c740516c64fae5ce8112c2fcfe1eaf3cb0d6
李兴广 <lixingguang@xunlei.com>
2022-12-14 16:53:11 +0800

    add coc notify

--- init.lua
+++ init.lua
@@ -501,3 +501,67 @@ cmd [[
 ]]
 
 require'modules.coc'
+
+-- coc.nvim notify
+local coc_status_record = {}
+
+function coc_status_notify(msg, level)
+  local notify_opts = { title = "LSP Status", timeout = 500, hide_from_history = true, on_close = reset_coc_status_record }
+  -- if coc_status_record is not {} then add it to notify_opts to key called "replace"
+  if coc_status_record ~= {} then
+    notify_opts["replace"] = coc_status_record.id
+  end
+  coc_status_record = vim.notify(msg, level, notify_opts)
+end
+
+function reset_coc_status_record(window)
+  coc_status_record = {}
+end
+
+local coc_diag_record = {}
+
+function coc_diag_notify(msg, level)
+  local notify_opts = { title = "LSP Diagnostics", timeout = 500, on_close = reset_coc_diag_record }
+  -- if coc_diag_record is not {} then add it to notify_opts to key called "replace"
+  if coc_diag_record ~= {} then
+    notify_opts["replace"] = coc_diag_record.id
+  end
+  coc_diag_record = vim.notify(msg, level, notify_opts)
+end
+
+function reset_coc_diag_record(window)
+  coc_diag_record = {}
+end
+
+cmd[[
+  function! s:DiagnosticNotify() abort
+    let l:info = get(b:, 'coc_diagnostic_info', {})
+    if empty(l:info) | return '' | endif
+    let l:msgs = []
+    let l:level = 'info'
+     if get(l:info, 'warning', 0)
+      let l:level = 'warn'
+    endif
+    if get(l:info, 'error', 0)
+      let l:level = 'error'
+    endif
+
+    if get(l:info, 'error', 0)
+      call add(l:msgs, ' Errors: ' . l:info['error'])
+    endif
+    if get(l:info, 'warning', 0)
+      call add(l:msgs, ' Warnings: ' . l:info['warning'])
+    endif
+    if get(l:info, 'information', 0)
+      call add(l:msgs, ' Infos: ' . l:info['information'])
+    endif
+    if get(l:info, 'hint', 0)
+      call add(l:msgs, ' Hints: ' . l:info['hint'])
+    endif
+    let l:msg = join(l:msgs, "\n")
+    if empty(l:msg) | let l:msg = ' All OK' | endif
+    call v:lua.coc_diag_notify(l:msg, l:level)
+  endfunction
+
+  autocmd User CocDiagnosticChange call s:DiagnosticNotify()
+]]
