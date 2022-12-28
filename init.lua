2bb8e791cd545881c5087daab7a13b20620daf1c
李兴广 <lixingguang@xunlei.com>
2022-12-28 13:36:05 +0800

    update

--- init.lua
+++ init.lua
@@ -105,7 +105,7 @@ require("lazy").setup({
   },
   {'aduros/ai.vim', lazy = true, cmd = 'AI'},
   -- {'dense-analysis/neural'},
-  {'mfussenegger/nvim-lint', config = function() require'modules.lint' end},
+  {'mfussenegger/nvim-lint', lazy = true, event = 'VeryLazy', config = function() require'modules.lint' end},
   -- ({
   --   "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
   --   lazy = true,
@@ -140,8 +140,7 @@ require("lazy").setup({
   {'glepnir/lspsaga.nvim', lazy = true, event = 'VeryLazy', branch = 'main', config = function() require 'modules.saga' end},
   {'weilbith/nvim-code-action-menu', lazy = true, cmd = 'CodeActionMenu'},
   'onsails/lspkind-nvim',
-  -- {'jose-elias-alvarez/null-ls.nvim', lazy = true, event = 'VeryLazy',
-  --   config = function() require 'modules.null-ls' end },
+  -- {'jose-elias-alvarez/null-ls.nvim', lazy = true, event = 'VeryLazy', config = function() require 'modules.null-ls' end },
   -- {"rcarriga/nvim-dap-ui", lazy = true, event = 'VeryLazy', dependencies = { "mfussenegger/nvim-dap"},
   -- config = function() require 'modules.dap' end},
   -- {'j-hui/fidget.nvim', event = 'VeryLazy', config = function() require('fidget'):setup() end}, -- 用noice代替
@@ -509,7 +508,7 @@ g.markdown_fenced_language = {
 g.markdown_fenced_languages = { "javascript", "typescript", "bash", "lua", "go", "rust", "c", "cpp" }
 
 --theme
-cmd 'colorscheme vscode'
+cmd 'colorscheme base16-ayu-dark'
 
 -- editorconfig-vim
 g.EditorConfig_exclude_patterns = { 'fugitive://.*', 'scp://.*', '' }
