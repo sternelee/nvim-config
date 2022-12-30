bff2bf75ce80625fb50bbbd024eb2569dd6e3b75
李兴广 <lixingguang@xunlei.com>
2022-12-30 14:10:21 +0800

    review

--- lua/modules/null-ls.lua
+++ lua/modules/null-ls.lua
@@ -2,9 +2,9 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
     sources = {
-        null_ls.builtins.code_actions.eslint_d,
-        null_ls.builtins.diagnostics.eslint_d,
-        null_ls.builtins.formatting.eslint_d,
+        -- null_ls.builtins.code_actions.eslint_d,
+        -- null_ls.builtins.diagnostics.eslint_d,
+        -- null_ls.builtins.formatting.eslint_d,
         null_ls.builtins.diagnostics.markdownlint,
         -- null_ls.builtins.diagnostics.cspell,
         null_ls.builtins.formatting.prettierd.with({
