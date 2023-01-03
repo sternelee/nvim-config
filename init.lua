0c8ba961aaef961c703ef8bce912e1305dfa148f
李兴广 <lixingguang@xunlei.com>
2023-01-03 09:52:41 +0800

    review

--- init.lua
+++ init.lua
@@ -377,8 +377,18 @@ autocmd({ "TextYankPost" }, {
     group = autogroup("highlight_yank", { clear = true }),
 })
 
+autocmd('BufReadPost', {
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
 -- 自动保存
--- require'modules.auto-save'
+require'modules.auto-save'
 
 local numbers = {"1", "2", "3", "4", "5", "6", "7", "8", "9"}
 for _, num in pairs(numbers) do
