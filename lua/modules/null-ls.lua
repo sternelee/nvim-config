18a492068291545115221378eb06e564da75037b
李兴广 <lixingguang@xunlei.com>
2023-02-27 12:02:08 +0800

    review

--- lua/modules/null-ls.lua
+++ lua/modules/null-ls.lua
@@ -6,7 +6,7 @@ null_ls.setup({
         -- null_ls.builtins.diagnostics.eslint_d,
         -- null_ls.builtins.formatting.eslint_d,
         null_ls.builtins.diagnostics.markdownlint,
-        null_ls.builtins.diagnostics.cspell,
+        -- null_ls.builtins.diagnostics.cspell,
         null_ls.builtins.code_actions.cspell,
         -- null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.prettierd
