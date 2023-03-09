3569ac31e29b7af69090e11cddbe289708653754
李兴广 <lixingguang@xunlei.com>
2023-03-09 20:39:16 +0800

    feat: format

--- lua/lsp/function.lua
+++ lua/lsp/function.lua
@@ -23,13 +23,14 @@ function M.toggle_format_on_save()
 end
 
 vim.api.nvim_create_user_command('LspToggleAutoFormat', 'lua require("lsp.function").toggle_format_on_save()', {})
+vim.api.nvim_create_user_command('Format', 'lua vim.lsp.buf.format({ async = true })', {})
 
 -- Custom textDocument/hover LSP handler to colorize colors inside hover results - WIP
 function M.custom_hover_handler(_, result)
   local handler = function(_, result)
     if result then
       local lines = vim.split(result.contents.value, '\n')
-      local bufnr = vim.lsp.util.open_floating_preview(lines, 'markdown', { border = EcoVim.ui.float.border or 'rounded' })
+      local bufnr = vim.lsp.util.open_floating_preview(lines, 'markdown', { border = 'rounded' })
       require('colorizer').highlight_buffer(bufnr, nil, vim.list_slice(lines, 2, #lines), 0, require('colorizer').get_buffer_options(0))
     end
   end
