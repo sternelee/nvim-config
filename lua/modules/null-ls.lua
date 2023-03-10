4a6266e7f3b6b8af3cd9b34089e60c0f8d012d1c
李兴广 <lixingguang@xunlei.com>
2023-03-10 14:30:22 +0800

    feat: review

--- lua/modules/null-ls.lua
+++ lua/modules/null-ls.lua
@@ -3,7 +3,8 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
     sources = {
-        null_ls.builtins.code_actions.eslint_d,
+        null_ls.builtins.code_actions.eslint,
+        -- null_ls.builtins.code_actions.eslint_d,
         null_ls.builtins.diagnostics.markdownlint,
         -- null_ls.builtins.diagnostics.cspell.with({
         --   extra_args = { "--config", vim.fn.expand("~/.config/nvim/cspell.json") },
