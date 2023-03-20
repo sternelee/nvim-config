347dfe074b173b045e563137b22b57d2acc81309
李兴广 <lixingguang@xunlei.com>
2023-03-20 10:25:35 +0800

    review

--- lua/modules/null-ls.lua
+++ lua/modules/null-ls.lua
@@ -1,21 +1,41 @@
 local null_ls = require("null-ls")
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
 
+-- https://livecode247.com/boost-your-neovim-experience-with-these-essential-plugins
+
+local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
+local code_actions = null_ls.builtins.code_actions
+local augroup = vim.api.nvim_create_augroup("NullForm", {})
+
 null_ls.setup({
     sources = {
-        null_ls.builtins.code_actions.eslint,
-        -- null_ls.builtins.code_actions.eslint_d,
-        null_ls.builtins.diagnostics.markdownlint,
-        -- null_ls.builtins.diagnostics.cspell.with({
+        code_actions.eslint,
+        -- code_actions.eslint_d,
+        diagnostics.markdownlint,
+        -- diagnostics.cspell.with({
         --   extra_args = { "--config", vim.fn.expand("~/.config/nvim/cspell.json") },
         -- }),
-        -- null_ls.builtins.code_actions.cspell.with({
+        -- code_actions.cspell.with({
         --   extra_args = { "--config", vim.fn.expand("~/.config/nvim/cspell.json") },
         -- }),
-        null_ls.builtins.diagnostics.codespell,
-        -- null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.prettierd,
-        -- null_ls.builtins.formatting.stylua,
-        -- null_ls.builtins.code_actions.gitsigns,
+        diagnostics.codespell,
+        formatting.prettier,
+        formatting.stylua,
+        -- formatting.prettierd,
+        diagnostics.eslint_d,
+        -- code_actions.gitsigns,
     },
+    on_attach = function(client, bufnr)
+      if client.supports_method("textDocument/formatting") then
+        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+        vim.api.nvim_create_augroup("BufWritePre", {
+          group = augroup,
+          buffer = bufnr,
+          callback = function()
+            vim.lsp.buf.format({ bufnr = bufnr })
+          end,
+        })
+      end
+    end,
 })
