d09483ec0364d078eae3271964df01dce59344c6
李兴广 <lixingguang@xunlei.com>
2023-03-22 14:04:42 +0800

    feat: 配置

--- init.lua
+++ init.lua
@@ -105,7 +105,7 @@ require('lazy').setup({
   },
   {'jose-elias-alvarez/typescript.nvim', ft = { 'typescript', 'typescriptreact', 'vue' }, config = function() require 'modules.typescript' end},
   'b0o/schemastore.nvim', -- json server
-  {'L3MON4D3/LuaSnip', dependencies = { 'rafamadriz/friendly-snippets' } },
+  {'L3MON4D3/LuaSnip', event = 'VeryLazy', dependencies = { 'rafamadriz/friendly-snippets' } },
   {'hrsh7th/nvim-cmp', dependencies = {
     'lukas-reineke/cmp-under-comparator',
     'petertriho/cmp-git',
@@ -119,7 +119,7 @@ require('lazy').setup({
     -- 'octaltree/cmp-look', -- 太多了
     -- 'dmitmel/cmp-digraphs',
     -- {'tzachar/cmp-tabnine', run='./install.sh'}, -- 内存占用太大
-  }},
+  }, config = function() require 'modules.cmp' end},
   -- {"roobert/tailwindcss-colorizer-cmp.nvim",
   -- config = function()
   --   require("tailwindcss-colorizer-cmp").setup({
@@ -545,131 +545,6 @@ notify.setup {
 }
 
 vim.notify = notify
-
-local lspkind = require('lspkind')
-require 'lspkind'.init()
-
-local cmp = require 'cmp'
-
-require("cmp_git").setup()
-local luasnip = require("luasnip")
-require("luasnip/loaders/from_vscode").lazy_load()
-
-local check_backspace = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
-cmp.setup({
-  completion = {
-    completeopt = 'menu,menuone,noinsert'
-  },
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end
-  },
-  mapping = {
-    ["<C-p>"] = cmp.mapping.select_prev_item(),
-    ["<C-n>"] = cmp.mapping.select_next_item(),
-    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<C-e>"] = cmp.mapping.close(),
-    ["<CR>"] = cmp.mapping(cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Insert }), {'i','s'}),
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expandable() then
-        luasnip.expand()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif check_backspace() then
-        cmp.complete()
-      else
-        fallback()
-      end
-    end, {
-      "i",
-      "s"
-    }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, {
-      "i",
-      "s"
-    }),
-  },
-  sources = {
-    { name = 'nvim_lsp', priority = 8 },
-    { name = 'luasnip', priority = 7 },
-    { name = 'buffer', option = { keyword_length = 2 } },
-    { name = 'nvim_lsp_signature_help' },
-    { name = 'calc' },
-    { name = 'emoji' },
-    -- { name = 'spell' },
-    -- { name = 'cmp_tabnine' },
-    { name = 'git' },
-    -- { name = 'digraphs' },
-    -- { name = 'treesitter' },
-    -- { name = 'look', keyword_length=4, option={convert_case=true, loud=true}},
-  },
-  formatting = {
-    format = lspkind.cmp_format({
-      mode = 'symbol_text', -- show only symbol annotations
-      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-      before = function(entry, vim_item)
-        return vim_item
-      end
-    })
-  },
-  -- formatting =  {
-  --   format = require("tailwindcss-colorizer-cmp").formatter
-  -- },
-  sorting = {
-    comparators = {
-      cmp.config.compare.offset,
-      cmp.config.compare.exact,
-      cmp.config.compare.score,
-      require "cmp-under-comparator".under,
-      cmp.config.compare.kind,
-      cmp.config.compare.sort_text,
-      cmp.config.compare.length,
-      cmp.config.compare.order,
-    },
-  },
-  flags = {
-    debounce_text_changes = 150,
-  },
-  window = {
-    completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered()
-  },
-  experimental = {
-    ghost_text = true
-  }
-})
-
--- for autopairs
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-cmp.event:on(
-  'confirm_done',
-  cmp_autopairs.on_confirm_done()
-)
-
-cmp.setup.filetype('gitcommit', {
-  sources = cmp.config.sources({
-    { name = 'git' },
-  }, {
-    { name = 'buffer' },
-  })
-})
 
 -- LSP config
 require('lsp/config')
