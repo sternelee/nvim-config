b35b515bc794c0039fdec65a794fac503e0d7830
李兴广 <lixingguang@xunlei.com>
2023-04-06 16:31:31 +0800

    feat: lualine样式

--- lua/modules/lualine.lua
+++ lua/modules/lualine.lua
@@ -18,8 +18,9 @@ end
 require('lualine').setup({
   options = {
     icons_enabled = true,
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
+    component_separators = '',
+    -- component_separators = { left = '', right = ''},
+    -- section_separators = { left = '', right = ''},
     disabled_filetypes = {},
     always_divide_middle = true,
     globalstatus = false,
