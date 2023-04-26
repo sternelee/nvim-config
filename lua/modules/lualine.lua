a66844fb6b1bf3b49eb4ecb59925433ebd11f700
李兴广 <lixingguang@xunlei.com>
2023-04-26 10:07:45 +0800

    feat: lualine lsp progress

--- lua/modules/lualine.lua
+++ lua/modules/lualine.lua
@@ -1,7 +1,26 @@
+-- lualine config
+
+local lsp_progress = function()
+	local messages = vim.lsp.util.get_progress_messages()
+	if #messages == 0 then
+		return ""
+	end
+	local status = {}
+	for _, msg in pairs(messages) do
+		table.insert(status, (msg.percentage or 0) .. "%% " .. (msg.title or ""))
+	end
+	local spinners = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+	local ms = vim.loop.hrtime() / 1000000
+	local frame = math.floor(ms / 120) % #spinners
+	return spinners[frame + 1] .. " " .. table.concat(status, " | ")
+end
+
 require("lualine").setup({
 	options = {
 		icons_enabled = true,
 		component_separators = "",
+		-- component_separators = { left = '', right = ''},
+		-- section_separators = { left = '', right = ''},
 		disabled_filetypes = {},
 		always_divide_middle = true,
 		globalstatus = true,
@@ -17,6 +36,7 @@ require("lualine").setup({
 			"diagnostics",
 			"selectioncount",
 			"searchcount",
+			lsp_progress,
 		},
 		lualine_x = { "encoding", "fileformat", "filetype" },
 		lualine_y = { "progress" },
