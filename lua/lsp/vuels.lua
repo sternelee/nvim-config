69c95616af4dd2722bb1e4a42668897a66fd9494
李兴广 <lixingguang@xunlei.com>
2023-05-06 13:56:57 +0800

    fix: vue

--- lua/lsp/vuels.lua
+++ lua/lsp/vuels.lua
@@ -1,27 +1,63 @@
 local M = {
   settings = {
     vetur = {
+      ignoreProjectWarning = false,
+      useWorkspaceDependencies = false,
       completion = {
         autoImport = true,
         useScaffoldSnippets = true,
+        scaffoldSnippetSources = {
+            workspace = "💼",
+            user = "🗒️",
+            vetur = "✌"
+        },
+        tagCasing = "kebad"
+      },
+      grammar = {
+        customBlocks = {
+          docs = "md",
+            i18n = "json"
+        }
       },
       format = {
+        enable = true,
+        options = {
+          tabSize = 2,
+          useTabs = false,
+        },
         defaultFormatter = {
-          html = "none",
+          html = "prettier",
           js = "prettier",
           ts = "prettier",
+          css = "prettier",
+          postcss = "prettier",
+          scss = "prettier",
+          sass = "prettier",
         },
       },
       validation = {
         template = true,
         script = true,
         style = true,
-        templateProps = true,
+        templateProps = false,
         interpolation = true,
       },
-      experimental = {
-        templateInterpolationService = true,
+      languageFeatures = {
+        updateImportOnFileMove = true,
       },
+      trace = {
+        server = "off",
+      },
+      dev = {
+        vlsPort = -1,
+        logLevel = "INFO"
+      },
+      experimental = {
+        templateInterpolationService = false,
+      },
+      underline = {
+        refValue = true
+      }
     },
   },
 }
