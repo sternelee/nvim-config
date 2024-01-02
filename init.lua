d446e29e8056dd00eab56783cb7cc26de6b27c6b
李兴广 <lixingguang@xunlei.com>
2024-01-02 12:14:04 +0800

    feat: review keymap

--- init.lua
+++ init.lua
@@ -14,22 +14,22 @@ g.loaded_perl_provider = 0
 g.mapleader = " "
 g.maplocalleader = ","
 
--- vim.o.guifont = "FiraCode Nerd Font Mono Light:h18"
+vim.o.guifont = "FiraCode Nerd Font Mono Light:h18"
 
--- local alpha = function()
---     return string.format("%x", math.floor(255 * vim.g.transparency or 0.8))
--- end
--- -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
--- g.neovide_transparency = 1
--- g.transparency = 0.9
--- g.neovide_background_color = "#0f1117" .. alpha()
--- g.neovide_scale_factor = 1.0
--- g.neovide_input_macos_alt_is_meta = 1
--- g.neovide_input_use_logo = 1
--- g.neovide_remember_window_size = 1
--- g.neovide_confirm_quit = 1
--- g.neovide_hide_mouse_when_typing = 0
--- g.neovide_cursor_vfx_mode = "ripple"
+local alpha = function()
+    return string.format("%x", math.floor(255 * vim.g.transparency or 0.8))
+end
+-- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
+g.neovide_transparency = 1
+g.transparency = 0.9
+g.neovide_background_color = "#0f1117" .. alpha()
+g.neovide_scale_factor = 1.0
+g.neovide_input_macos_alt_is_meta = 1
+g.neovide_input_use_logo = 1
+g.neovide_remember_window_size = 1
+g.neovide_confirm_quit = 1
+g.neovide_hide_mouse_when_typing = 0
+g.neovide_cursor_vfx_mode = "ripple"
 
 cmd([[
   filetype plugin on
@@ -245,7 +245,7 @@ require("lazy").setup({
     keys = {
       {
         mode = { "v", "n" },
-        "<Leader>m",
+        "<Leader>M",
         "<cmd>MCstart<cr>",
         desc = "Create a selection for selected text or word under the cursor",
       },
@@ -840,7 +840,7 @@ map("n", "fc", "<cmd>Telescope commands<CR>")
 map("n", "fe", "<cmd>Telescope file_browser<CR>")
 map("n", "<leader>f", "<cmd>Telescope find_files<CR>")
 map("n", "<leader>b", "<cmd>Telescope buffers<CR>")
-map("n", "<leader>m", "<cmd>Telescope marks<CR>")
+-- map("n", "<leader>m", "<cmd>Telescope marks<CR>")
 map("n", "<leader>m", "<cmd>ReachOpen marks<CR>")
 -- map('n', '<leader>/', '<cmd>Telescope live_grep<CR>')
 map("n", "<leader>'", "<cmd>Telescope resume<CR>")
